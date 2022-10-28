1900256b45076b5267e815748e7c5579dc7e30d1
eahb0y <getelementby111@gmail.com>
2022-10-28 21:59:35 +0500

    initial page

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const XylophonePage());
+}
+
+class XylophonePage extends StatelessWidget {
+  const XylophonePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
