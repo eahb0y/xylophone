f56992a25c5a4d9e1beff9a26a1b315abc715a54
eahb0y <getelementby111@gmail.com>
2022-10-31 21:30:23 +0500

    xylophone is created

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,6 +10,48 @@ class XylophonePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: Xylophone(),
+      ),
+    );
+  }
+}
+
+class Xylophone extends StatelessWidget {
+  const Xylophone({Key? key}) : super(key: key);
+
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+  Expanded getSoundButton(Color color, int soundNumber) {
+    return Expanded(
+        child: TextButton(
+      onPressed: () {
+        playSound(soundNumber);
+      },
+      child: Container(
+        color: color,
+      ),
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Column(
+      children: [
+        getSoundButton(Colors.red, 1),
+        getSoundButton(Colors.orange, 2),
+        getSoundButton(Colors.yellow, 3),
+        getSoundButton(Colors.green, 4),
+        getSoundButton(Colors.lightBlueAccent, 5),
+        getSoundButton(Colors.blue, 6),
+        getSoundButton(Colors.purple, 7),
+      ],
+    ));
   }
 }
